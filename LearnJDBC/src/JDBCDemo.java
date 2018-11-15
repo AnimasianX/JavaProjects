d684a0e411ff47ff1aa15f746efa6338e9f54250
Jimmy Lin <jlin2744@gmail.com>
2018-11-15 14:07:10 -0500

    Add files via upload

--- LearnJDBC/src/JDBCDemo.java
+++ LearnJDBC/src/JDBCDemo.java
@@ -14,26 +14,44 @@ public class JDBCDemo {
 		// TODO Auto-generated method stub
 		//we have to initialize the db we are going to use.
 		String url = "jdbc:mysql://127.0.0.1:3306/employees_database";
+		int rowsAffected;
 		try {
 			//getconnection takes in 3 argument, the url, username and password of the connection we are attempting to connect to.
 			//establish connection object
-			Connection conn = DriverManager.getConnection(url, "root", "oopsie-daisy");
+			Connection conn = DriverManager.getConnection(url, "root", "Jimster2744");
 			
 			//create a statement object to sent to the database
 			Statement statement = conn.createStatement();
 			
-			//execute statement object
+			
+			//rowsAffected = statement.executeUpdate("INSERT INTO employees_tbl(id, name, dept, salary)values(1001,'Dark','CIO',10000);");
+			//deletes all data
+			//rowsAffected = statement.executeUpdate("DELETE FROM employees_tbl");
+			//updates data
+			rowsAffected = statement.executeUpdate("UPDATE employees_tbl\n" + 
+					"SET salary = 50000\n" + 
+					"WHERE name = 'John';");
+			
+			//execute statement object//cannot use this to manipulate data set.
+			//exclusively to query data from the desired db
 			ResultSet resultSet = statement.executeQuery("SELECT * FROM employees_tbl");
 			
+			//to insert to the table, we must use executeUpdate.
+			
+			
+			int salaryTotal = 0;
 			//process result
 			while(resultSet.next()) {
+				//gets number from salary column
+				salaryTotal += resultSet.getInt("salary");
 				//getting department column
 				//getting name colum
 				//you could also do getInt if you want to get a column that is made up of integers.
 				System.out.println(resultSet.getString("dept"));
 			}
+			System.out.println(salaryTotal);
+			System.out.println("Rows Affected: " + rowsAffected);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
